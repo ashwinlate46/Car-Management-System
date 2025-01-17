6e8760cf6fce9210368ae3a95bbaba639ba11719
Ashwin Late <157834243+ashwinlate46@users.noreply.github.com>
2025-01-17 19:14:51 +0530

    Delete Car-Management-System-main directory

--- Car-Management-System-main/Java Backend/Car_Management/src/main/java/com/carApi/Car_Management/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.carApi.Car_Management.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.carApi.Car_Management.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Integer>{
-
-}
